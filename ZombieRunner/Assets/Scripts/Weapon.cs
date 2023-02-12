7f41047414649a7f20d81a0bed8b9b284fb5b95e
selinihtyr <nurselinihtiyar@gmail.com>
2023-02-13 00:08:55 +0300

    183-188

--- ZombieRunner/Assets/Scripts/Weapon.cs
+++ ZombieRunner/Assets/Scripts/Weapon.cs
@@ -11,9 +11,15 @@ public class Weapon : MonoBehaviour
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] GameObject hitEffect;
     [SerializeField] ammo ammoSlot;
+    [SerializeField] AmmoType ammoType;
     [SerializeField] float timeBetweenShots = 0.5f;
 
     bool canShoot = true;
+
+    private void OnEnable()
+    {
+        canShoot = true;
+    }
 
     void Update()
     {
@@ -26,11 +32,11 @@ public class Weapon : MonoBehaviour
     IEnumerator Shoot()
     {       
         canShoot = false;
-        if(ammoSlot.GetCurrentAmmo() > 0)
+        if(ammoSlot.GetCurrentAmmo(ammoType) > 0)
         {
             PlayMuzzleFlash();
             ProcessRaycast();
-            ammoSlot.ReduceCurrentAmmo();
+            ammoSlot.ReduceCurrentAmmo(ammoType);
         }
         yield return new WaitForSeconds(timeBetweenShots);
         canShoot = true;
