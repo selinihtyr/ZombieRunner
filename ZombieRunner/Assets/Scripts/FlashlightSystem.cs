b537c72b3ebbb99f7f4bd4a3f558b7a09dee1562
selinihtyr <nurselinihtiyar@gmail.com>
2023-02-14 13:14:58 +0300

    189-203

---
+++ ZombieRunner/Assets/Scripts/FlashlightSystem.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DecreaseLightAngel();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        myLight.spotAngle = restoreAngle;
+    }
+
+    public void RestoreLightIntersity(float intensityAmount)
+    {
+        myLight.intensity += intensityAmount;
+    }
+    
+    private void DecreaseLightAngel()
+    {
+        if(myLight.spotAngle <= minimumAngle)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+}
